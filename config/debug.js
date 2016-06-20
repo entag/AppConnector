e128f8491a42a482a78e0137d15da52ce18f0ac5
Josh <de4thNite@gmail.com>
2016-06-20 15:52:40 +1000

    fix admin check

---
+++ config/debug.js
@@ -0,0 +1,3 @@
+module.exports = {
+	doNotPostToConnectwise: true
+}
--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,7 @@ var routeAuth = require('../controllers/routeAuth');
 
 //admin page
 
-router.get('/', routeAuth.forceAdmin(), function (req, res) {
+router.get('/', routeAuth.forceAdmin, function (req, res) {
 		return res.render('admin', {
 			user: req.user
 			})
