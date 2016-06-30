b5b161cbec98e947d5be675b105b9e196941e02f
Josh <de4thNite@gmail.com>
2016-06-30 11:05:23 +1000

    user feedback system

---
+++ public/js/login.js
@@ -0,0 +1,33 @@
+$(function() {
+	$('#login').on('submit', function(e) {
+		e.preventDefault();
+		
+		// validation check
+		if(!$('#email').val() ^ !$('#password').val()){
+			// invalid
+			$.notify('Please enter a username and password', 'error');
+			return
+		}
+
+		$('#btn_login').addClass('disabled'); //disable button
+
+		$.ajax({
+			method: 'POST',
+			url: 'auth/signin',
+			data: {
+				email: $('#email').val(),
+				password: $('#password').val()
+			}
+		})
+		.success(function(res){
+			// redirect
+			$.notify('Success', 'success');
+			window.location = '/';
+		})
+		.error(function(){
+			// flash error message
+			$.notify('Invalid login credentials', 'error');
+			$('#btn_login').removeClass('disabled');
+		})
+	})
+})
